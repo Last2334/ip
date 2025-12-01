b4add516ea9e649323adf871b393cda81e5765d9
TestUser <testuser@example.com>
2025-12-02 05:35:25 +0800

    update-json

--- api/index.js
+++ api/index.js
@@ -2,7 +2,6 @@ export default function handler(req, res) {
   // 从请求头中获取真实 IP
   const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
   
-  // 只需要接口，返回纯文本 IP
-  // 如果需要 JSON 格式，可以改为: res.status(200).json({ ip: ip });
-  res.status(200).send(ip);
+  // 返回 JSON 格式的 IP
+  res.status(200).json({ ip: ip });
 }
